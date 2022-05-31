12883fb9eea6d1eb91b4116e7b04860ea871437d
recipra <jmo0425@outlook.com>
2022-05-31 16:43:00 -0400

    add addButton event listener

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 const todoItem = document.getElementById('todo-item')
 const addButton = document.getElementById('submit-button')
 const todoList = document.getElementById('todo-list')
+
+addButton.addEventListener('click', event => {
+  const li = document.createElement('li')
+  li.textContent = todoItem.value
+  todoList.appendChild(li)
+  todoItem.value = ''
+})
