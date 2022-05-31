57579676b99e383d48aab38bbae4f173364c31c0
recipra <jmo0425@outlook.com>
2022-05-31 16:48:40 -0400

    add form to html and js to allow "enter" key for input

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
+const form = document.querySelector('form')
 const todoItem = document.getElementById('todo-item')
 const addButton = document.getElementById('submit-button')
 const todoList = document.getElementById('todo-list')
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+})
 
 addButton.addEventListener('click', event => {
   const li = document.createElement('li')
